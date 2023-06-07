049be7c84a2884703cd7d013d27b1239f2420697
audris <audriskasula@gmail.com>
2023-06-07 10:32:01 +0700

    porto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProjectPageComponent } from './pages/project-page/project-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:HomePageComponent},
+  {path: 'project', component:ProjectPageComponent},
+  {path: 'about-me', component:AboutPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProjectPageComponent } from './pages/project-page/project-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomePageComponent,
+    ProjectPageComponent,
+    AboutPageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
